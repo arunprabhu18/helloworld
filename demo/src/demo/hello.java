339e932dccf18ac00ab4df16bc2c2d6e0ab15426
arunprabhu18 <54343118+arunprabhu18@users.noreply.github.com>
2019-08-29 07:48:08 +0530

    Update hello.java

--- demo/src/demo/hello.java
+++ demo/src/demo/hello.java
@@ -9,7 +9,7 @@ public class hello {
 		System.out.println("git to jenkins");
 		System.out.println("git to jenkins");
 		System.out.println("git to jenkins");
-		System.out.println("git to jenkins");
+		
 	System.out.println("git to jenkins");
 	
 
